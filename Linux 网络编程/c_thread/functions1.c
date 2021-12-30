c94695adc265c7fb48ad9bb1d34a837617ddc9bb
David Zheng <19953656355@163.com>
2021-12-30 11:10:13 +0800

    added emphasis information

--- Linux 网络编程/c_thread/functions1.c
+++ Linux 网络编程/c_thread/functions1.c
@@ -8,9 +8,14 @@
 void *pth_fun(void *pth_arg);
 void signal_catch(int signal_index);
 
-//线程编号
-pthread_t t_id=0;
-
+//次线程信息
+typedef struct pthread_arg
+{
+    pthread_t tid;//线程号
+    int phono;//我自己定义的编号
+    int fd;//文件描述符
+}ptharg; 
+ptharg pth;
 int main(void)
 {
     //用普通函数来单线运行
@@ -19,12 +24,16 @@ int main(void)
     //用线程函数来并发运行
     /*int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
                           void *(*start_routine) (void *), void *arg);*/
-    pthread_create(&t_id,NULL,pth_fun,NULL);
-
-    //定时5秒，时间到了取消次线程
+    pthread_create(&pth.tid,NULL,pth_fun,NULL);
+    
+    //获取当前函数的线程号，并结束当前线程的运行
+    //int pthread_cancel(pthread_t thread);
+    //pthread_cancel(pthread_self());
+    pthread_exit(NULL);
+    //定时5秒，时间到了取消次线
     signal(SIGALRM,signal_catch);
     alarm(5);
-
+    //return 0;
     while (1)
     {
         printf("2222\n");
@@ -36,9 +45,11 @@ int main(void)
 }
 void *pth_fun(void *pth_arg)
 {
+    int i=1;
     while(1)
     {
-        printf("1111\n");
+        printf("%d\n",i);
+        i++;
         sleep(1);
     }
 
@@ -49,6 +60,6 @@ void signal_catch(int signal_index)
     if(signal_index==SIGALRM)
     {
         //取消线程
-        pthread_cancel(t_id);
+        pthread_cancel(pth.tid);
     }
 }
