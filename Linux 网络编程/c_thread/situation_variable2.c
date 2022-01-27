fa97d502e88af00727be2684e30e8054f17afd62
David Zheng <19953656355@163.com>
2022-01-27 08:03:00 +0800

    end sv2

--- Linux 网络编程/c_thread/situation_variable2.c
+++ Linux 网络编程/c_thread/situation_variable2.c
@@ -3,24 +3,36 @@
 #include <unistd.h>
 #include <pthread.h>
 #include<errno.h>
+#include<signal.h>
 
 //线程函数
 void *pth_fun(void *pth_arg);
+//设置中断处理函数
+void signal_catch(int signal_index);
 //出错处理函数
 //print_err(char *str,int line,)
+
+//定义va
+int va=0;
 //定义条件变量
 pthread_cond_t cond;
 //直接初始化
 //pthread_cond_t cond=PTHREAD_COND_INITIALIZER;
+
 //定义互斥锁,并直接初始化它
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
 
+//定义次线程编号
+pthread_t t_id=0;
+
+
 int main(void)
 {
-    pthread_t t_id=0;
+    //设置中断处理函数
+    signal(SIGINT,signal_catch);
 
     //初始化条件变量
-    cond_init_ret=pthread_cond_init(&cond,NULL);
+    pthread_cond_init(&cond,NULL);
     pthread_create(&t_id,NULL,pth_fun,NULL);
 
     while (1)
@@ -45,12 +57,24 @@ void *pth_fun(void *pth_arg)
     
     while(1)
     {
-        if(va==5)
+        pthread_mutex_lock(&mutex);
+        /*之所以将mutex传递给该函数，是害怕休眠后导致互斥锁没有解开，
+        使得主线程无法加锁，无法按照计划正常运行。*/
+        pthread_cond_wait(&cond,&mutex);
         {
             printf("va=%d\n",va);
             va=0;
         }
+        pthread_mutex_unlock(&mutex);
     }
 
     return NULL;
+}
+
+void signal_catch(int signal_index)
+{
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond);
+
+    exit(0);
 }
