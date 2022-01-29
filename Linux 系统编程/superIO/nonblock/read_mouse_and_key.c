75bfef27b16cc29c689e5130154f9cd1fa152b6c
David Zheng <19953656355@163.com>
2022-01-29 19:58:58 +0800

    added read mouse and key

---
+++ Linux 系统编程/superIO/nonblock/read_mouse_and_key.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<string.h>
+#include<errno.h>
+//出错处理函数
+void print_err(char *str,int line,int err_no);
+void read_key();
+void read_mouse();
+
+int main(void)
+{
+    read_mouse();
+
+    return 0;
+}
+void print_err(char *str,int line,int err_no)
+{
+    printf("%d,%s:%s\n",line,str,strerror(err_no));
+
+    exit(-1);
+}
+void read_key()
+{
+    char buf[100]={0};
+    int read_ret=0;
+
+    while(1)
+    {
+        printf("111\n");
+        //read返回读到的字节数
+        read_ret=read(0,buf,sizeof(buf));
+        if(read_ret>0)
+        {
+            printf("%s",buf);
+        }
+        printf("222\n\n"); 
+    }       
+}
+void read_mouse()
+{
+    int mousefd=-1;
+    int mouse_addr;
+    int read_ret;
+
+    mousefd=open("/dev/input/mouse3",O_RDONLY);
+    if(mousefd==-1)
+    {
+        print_err("open mouse fail",__LINE__-3,errno);
+    }
+    while (1)
+    {
+        printf("111\n");
+        //read返回读到的字节数
+        read_ret=read(mousefd,&mouse_addr,sizeof(mouse_addr));
+        if(read_ret>0)
+        {
+            printf("%d\n",mouse_addr);
+        }
+        printf("222\n\n"); 
+    }
+    
+}
