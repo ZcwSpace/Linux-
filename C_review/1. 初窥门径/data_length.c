79c95fdf4ec59753147469ebfce6725bf4917e28
David Zheng <19953656355@163.com>
2022-03-19 07:59:11 +0800

    added 条件判断

---
+++ C_review/1. 初窥门径/data_length.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("int:%d\n",sizeof(int));
+    printf("short:%d\n",sizeof(short));
+    printf("float:%d\n",sizeof(float));
+
+    return 0;
+}
---
+++ C_review/1. 初窥门径/for_while.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    /*
+    for(i=0;i<10;i++)
+    {
+        if(i%2)
+        {
+            continue;
+        }
+    }
+    */
+    
+    i=0;
+    while(i<100)
+    {
+        if(i%2)
+        {
+            continue;
+        }
+        i++;
+    }
+    //*/
+    return 0;
+}
